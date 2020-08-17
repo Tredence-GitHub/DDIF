56c301af3b920a0d5fda251abb9307f2d69353ff
Nidhi Raghavendra <nidhi.raghavendra@tredence.com>
2020-08-17 13:29:09 +0530

    latest with bcrypt

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,51 +1,74 @@
 const db = require('../config/db.js');
 const express = require('express');
 const Router = express.Router();
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 Router.post('/authenticateUser', (req, res)=>{
     let request_data = req.body;
     db.Users.findAll({
-        where: {
-            username: request_data.username,
-            password: request_data.password
-        }
-    }).then((result)=>{
-        if(result.length > 0){
-            res.status(200).json({message: 'successfull', data: JSON.parse(JSON.stringify(result))});
-        }else{
-            res.status(400).json({message: 'You are not registered with us! Please register!'});
-        }
-    }).catch((err)=>{
-        res.status(400).json({message: 'Failed in login', error: [err]});
-    })
+            where: {
+                username: request_data.username,
+            }
+        }).then((result)=>{
+            if(result.length > 0){
+                bcrypt.compare(request_data.password, result[0].dataValues.password, (err, response) =>{
+                    if(response === true){
+                        res.status(200).json({message: 'successful', data: JSON.parse(JSON.stringify(result))});
+                    }else{
+                        console.log('FAILED TO MATCH !! ', err, res)
+                        res.status(400).json({message: 'You are not registered with us! Please register!'});
+                    }
+                })
+            }else{
+                res.status(400).json({message: 'You are not registered with us! Please register!'});
+            }
+        }).catch((err)=>{
+            console.log('ERROR -- ', err)
+            res.status(400).json({message: 'Failed in login', error: [err]});
+        })
+    
+      
 })
 
 Router.post('/forgotpassword', (req, res)=>{
-    db.Users.findAll({
-        where: {
-            username: request_data.username
-        }
-    }).then((result)=> {
-        if(result.length > 0){
-            db.Users.update({
-                password: request_data.password
-            },{
+    let request_data = req.body;
+   
+    bcrypt.hash(request_data.password, saltRounds, (err, hash) => {
+        // Now we can store the password hash in db.
+        if(!err)
+            {db.Users.findAll({
                 where: {
-                username: request_data.username
-             }
-            }).then((result)=>{
-                res.status(200).json({message: 'Password successfully changed!', data: []});
+                    username: request_data.username
+                }
+            }).then((result)=> {
+                if(result.length > 0){
+                    db.Users.update({
+                        password: hash
+                    },{
+                        where: {
+                        username: request_data.username
+                    }
+                    }).then((result)=>{
+                        res.status(200).json({message: 'Password successfully changed!', data: []});
+                    }).catch((err)=>{
+                        res.status(400).json({message: 'Failed to change password!', error: [err]});
+                    })
+                }
+                else{
+                    res.status(200).json({message: 'Username not found !'});
+                }
             }).catch((err)=>{
-                res.status(400).json({message: 'Failed to change password!', error: [err]});
+                console.log(err);
+                res.status(400).json({message: 'Failed to change password', error: [err]});
             })
         }
         else{
-            res.status(200).json({message: 'Username not found !'});
+            console.log(err);
+            res.status(400).json({message: 'Failed to encrypt password', error: [err]});
         }
-    }).catch((err)=>{
-        console.log(err);
-        res.status(400).json({message: 'Failed to change password', error: [err]});
-    })
+      });
 })
 
 
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,10 +1,12 @@
 const db = require('../config/db.js');
 const express = require('express');
 const Router = express.Router();
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 Router.post('/register', (req, res)=> {
     let request_data = req.body;
-
     db.Users.findAll({
         where:{
             username: request_data.username
@@ -14,13 +16,25 @@ Router.post('/register', (req, res)=> {
             res.status(200).json({message: 'Username is already taken! Please choose a different username or login!', data: []})
         }
         else{
-            db.Users.create(request_data).then((result)=>{
-                console.log(result);
-                res.status(200).json({message: 'Congratulations! You are registered with us!', data: [{username: request_data.username, name: request_data.name}]});
-            }).catch((err)=>{
-                console.log(err, 'Registration failed !');
-                res.status(400).json({message: 'Failed to register the user!', error: [err]});
-            })
+            bcrypt.hash(request_data.password, saltRounds, (err, hash) => {
+                // Now we can store the password hash in db.
+                if(!err){
+                    request_data.password = hash;
+
+                    db.Users.create(request_data).then((result)=>{
+                        console.log(result);
+                        res.status(200).json({message: 'Congratulations! You are registered with us!', data: [{username: request_data.username, name: request_data.name}]});
+                    }).catch((err)=>{
+                        console.log(err, 'Registration failed !');
+                        res.status(400).json({message: 'Failed to register the user!', error: [err]});
+                    })
+                }
+                else {
+                    console.log(err);
+                    res.status(400).json({message: 'Failed to register the user!', error: [err]});
+                }
+
+              });
         }
     }).catch((err)=>{
         console.log(err, 'Registration failed !');
